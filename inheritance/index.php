c664da5f8daf00bff6bbd277e6afe35f1187f8bf
Edozie Nneke <nneke.edozie@gmail.com>
2023-09-16 11:10:50 +0100

    implemented inheritance by inheriting properties of a super class

--- inheritance/index.php
+++ inheritance/index.php
@@ -34,8 +34,37 @@ class Person
 
 class Employee extends Person
 {
+  private $position;
+  private $salary;
+
+  public function __construct($employee_name, $employee_age, $position, $salary)
+  {
+    parent::__construct($employee_name, $employee_age);
+    $this->position = $position;
+    $this->salary = $salary;
+  }
+
+  public function getPostion()
+  {
+    return $this->position;
+  }
+
+  public function setPostion($newPosition)
+  {
+    $this->position = $newPosition;
+  }
+
+  public function getSalary()
+  {
+    return $this->salary;
+  }
+
+  public function setSalary($newSalary)
+  {
+    $this->salary = $newSalary;
+  }
 }
 
-$employeeOne = new Employee('Edozie', 23);
+$employeeOne = new Employee('John Doe', 23, 'Software Developer', 200000);
 
-echo 'I am a new employee and my name is ' . $employeeOne->name;
+echo 'I am a new employee and my name is ' . $employeeOne->name . '.<br> I am a ' . $employeeOne->getPostion() . ' and earn ' . 'N' . $employeeOne->getSalary();
